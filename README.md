173bd6ecdcd640036b67a5f3e0631944f0d6f7a8
sebasnop <sevalenciazapata@gmail.com>
2024-09-23 23:39:24 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# San Valentín
+
+**Completado por**:
+
+Sebastian Valencia Zapata
+
+[sevalenciaz@unal.edu.co](mailto:sevalenciaz@unal.edu.co)
---
+++ script.js
@@ -0,0 +1,17 @@
+const message = document.querySelector('#message');
+const buttons = document.querySelector('#buttons');
+const noBtn = document.querySelector('.no');
+const yesBtn = document.querySelector('.yes');
+
+noBtn.addEventListener('click', () => {
+    //RANDOM POSITIONIN OF THE noBTn
+    //HINT # 1: window has properties of the width and height of the screen
+    //HINT # 2: offsetWidth and offsetHeight are properties of html elements
+    //HINT # 3: This button has position 'absolute', which means that its position in the html document could
+    //be given by the style properties left and top (in px values)
+});
+
+yesBtn.addEventListener('click', () => {
+    message.innerHTML = 'TE AMO MIBIDA';
+    buttons.style.display = 'none';
+});
